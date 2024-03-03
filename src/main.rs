932d07e8493e92a0aebadf759d4012ce50d3eeb7
jcasben <jcasben02@gmail.com>
2024-03-03 19:51:16 +0100

    [UPDATE] Added new routes

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,19 @@ mod repository;
 #[macro_use] 
 extern crate rocket;
 
-use api::cube_api::insert_cube;
+use api::cube_api::{insert_cube, get_cube, delete_cube, update_cube, get_all_cubes};
 use repository::mongodb_repo::MongoRepo;
-
 
 #[launch]
 fn rocket() -> _ {
     let db = MongoRepo::init();
-    rocket::build().manage(db).mount("/", routes![insert_cube])
+    rocket::build()
+        .manage(db)
+        .mount("/", routes![insert_cube])
+        .mount("/", routes![get_cube])
+        .mount("/", routes![update_cube])
+        .mount("/", routes![delete_cube])
+        .mount("/", routes![get_all_cubes])
 }
 
 /*#[get("/")]
