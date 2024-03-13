174d03f67e2a81cad5a90ecae104eccfbe73dc43
jcasben <jcasben02@gmail.com>
2024-03-13 20:32:47 +0100

    Update(main.rs): added env variables for app port and address

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,22 @@ mod repository;
 #[macro_use] 
 extern crate rocket;
 
+use std::env;
+use dotenv::dotenv;
 use api::cube_api::*;
 use repository::mongodb_repo::MongoRepo;
 
 #[launch]
 fn rocket() -> _ {
+    dotenv().ok();
+    let port: u16 = env::var("PORT")
+        .unwrap()
+        .parse()
+        .expect("PORT not found in .env");
+    let address = env::var("ADDRESS").unwrap_or_else(|_| "ADDRESS not found in .env".to_string());
     let db = MongoRepo::init();
     rocket::build()
-        .configure(rocket::Config::figment().merge(("port", 10000)).merge(("address", "0.0.0.0")))
+        .configure(rocket::Config::figment().merge(("port", port)).merge(("address", address)))
         .manage(db)
         .mount("/", routes![insert_cube])
         .mount("/", routes![get_cube])
